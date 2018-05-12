64fd721abfe1cc48f30efb2b87b904682fe2d48f
Raül Pérez <repejota@gmail.com>
2018-05-12 18:16:28 +0200

    Initial UI

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,15 +1,53 @@
 <template>
-  <div class="home">
-    <h1>{{ msg }}</h1>
+  <div class="container home">
+    <div class="row">
+      <div class="col">
+        <h1>Welcome to issues2markdown</h1>
+
+        <section class="search">
+          <div class="row">
+            <div class="col">
+              <h4>Query Issues:</h4>
+              <div class="input-group">
+                <input class="form-control border-secondary" type="search" placeholder="Query issues...">
+                <div class="input-group-append">
+                    <button class="btn btn-outline-secondary" type="button">
+                        <i class="fa fa-search"></i>
+                    </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+
+        <section class="results">
+          <div class="row">
+            <div class="col">
+              <h4>Markdown Result:</h4>
+              <pre><code class="markdown">{{ markdown }}</code></pre>
+            </div>
+          </div>
+        </section>
+
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+const markdownResult = `- [ ] org/repo : [Issue Title](https://github.com/org/repo/issues/1)
+- [ ] org/repo : [Issue Title](https://github.com/org/repo/issues/1)
+- [ ] org/repo : [Issue Title](https://github.com/org/repo/issues/1)
+- [ ] org/repo : [Issue Title](https://github.com/org/repo/issues/1)
+- [ ] org/repo : [Issue Title](https://github.com/org/repo/issues/1)
+- [ ] org/repo : [Issue Title](https://github.com/org/repo/issues/1)`
+
 export default {
   name: 'Home',
   data () {
     return {
-      msg: 'Welcome to issues2markdown'
+      markdown: markdownResult,
+      query: ''
     }
   }
 }
@@ -17,18 +55,5 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+
 </style>
